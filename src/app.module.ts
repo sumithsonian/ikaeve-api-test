6bec61b6621ba38253c926c8f6a0f2ac5d6ef4d8
すみそ <sumi@wattai.jp>
2022-07-03 21:54:11 +0900

    add: controller雛型追加

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,19 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersController } from './users/users.controller';
+import { TeamsController } from './teams/teams.controller';
+import { TournamentsController } from './tournaments/tournaments.controller';
+import { TournamentSeriesController } from './tournament-series/tournament-series.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, UsersController],
+  controllers: [
+    AppController,
+    UsersController,
+    TeamsController,
+    TournamentsController,
+    TournamentSeriesController,
+  ],
   providers: [AppService],
 })
 export class AppModule {}
