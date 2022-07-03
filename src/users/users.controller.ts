273a2f1f9026782b40f6c1d0d50ac275e1475473
すみそ <sumi@wattai.jp>
2022-07-03 21:46:15 +0900

    update: リクエストメソッド指定追加

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,9 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @Get()
+  findAll(): string {
+    return 'This action returns all users';
+  }
+}
