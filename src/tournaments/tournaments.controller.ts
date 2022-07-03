c901a7462b4baeff3142abc3ebd8def1e93965c8
すみそ <sumi@wattai.jp>
2022-07-04 00:23:29 +0900

    add: ルーティングメソッド追加

--- src/tournaments/tournaments.controller.ts
+++ src/tournaments/tournaments.controller.ts
@@ -1,9 +1,37 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  // Query,
+  Post,
+  Put,
+  Param,
+  Delete,
+} from '@nestjs/common';
 
 @Controller('tournaments')
 export class TournamentsController {
+  @Post()
+  create(): string {
+    return 'This action adds a new tournament';
+  }
+
   @Get()
   findAll(): string {
     return 'This action returns all tournaments';
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return `This action returns a #${id} tournament`;
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string) {
+    return `This action updates a #${id} tournament`;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return `This action removes a #${id} tournament`;
   }
 }
